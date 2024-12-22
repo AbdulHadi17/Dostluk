77b6f6785a1dad8e0594bd825c8deaffc1e4a6a1
AbdulHadi17 <hadisaqib2048@gmail.com>
2024-12-22 16:00:13 +0500

    A bit backend for Message

--- server/routes/Message.route.js
+++ server/routes/Message.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
+import { deleteMessage, getFullChat, sendMessage } from '../controllers/Message.controller.js';
 
 
 const Router = express.Router();
 
-// Router.route('/getUserChatroomsAndChats').get(isAuthenticated);
-// Router.route('/getUserChatroomsAndChats').post(isAuthenticated);
+Router.route('/sendMessage/:id').post(isAuthenticated,sendMessage);
+Router.route('/getFullChat/:id').get(isAuthenticated,getFullChat);
+Router.route('/deleteMessage/:id').delete(isAuthenticated,deleteMessage);
 
 
 export default Router;
